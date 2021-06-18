70bca2557325918a5357096b1009c5bd6a098b97
Funny-Code-d <sosnin_dienis@mail.ru>
2021-06-18 14:35:21 +0700

    Удалены лишние файлы

--- parser/test_time.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from time import sleep
-start = datetime.datetime.now()
-sleep(3)
-end = datetime.datetime.now()
-diff = end - start
-print("Разнциа во времени {} секунд {} микросекунд".format(diff.seconds, diff.microseconds))
