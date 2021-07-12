f162ff1b8f326142366da55cccd1b94321bc8e12
Denis <Funny-Code-d>
2021-07-12 22:20:12 +0700

    Работа по двум городам: Новосибирск и Иркутск

--- parser/treading.sh
+++ parser/treading.sh
@@ -1,11 +1,7 @@
 python3 main_class.py 1 first_step &
-python3 main_class.py 2 first_step &
-python3 main_class.py 3 first_step &
-python3 main_class.py 4 first_step &
-
+python3 main_class.py 2 first_step
+sleep 1000
 python3 main_class.py 1 swap
 
 python3 main_class.py 1 second_step &
-python3 main_class.py 2 second_step &
-python3 main_class.py 3 second_step &
-python3 main_class.py 4 second_step &
+python3 main_class.py 2 second_step
