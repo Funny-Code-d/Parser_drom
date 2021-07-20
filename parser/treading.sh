49af934918007abf8f6740c07c84de8aa8c08097
Denis <64072620+Funny-Code-d@users.noreply.github.com>
2021-07-20 07:12:49 +0700

    Delete treading.sh

--- parser/treading.sh
+++
@@ -1,7 +0,0 @@
-python3 main_class.py 1 first_step &
-python3 main_class.py 2 first_step
-sleep 1000
-python3 main_class.py 1 swap
-
-python3 main_class.py 1 second_step &
-python3 main_class.py 2 second_step
